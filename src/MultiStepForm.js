7737d07cadecf79f724258946dffa57a4c115beb
Itamar Tati <itamar.tati1994@gmail.com>
2019-08-22 16:33:15 +0100

    added store(reducers)

---
+++ src/MultiStepForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class WizardForm extends Component {
+
+    render() {
+
+      return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+      );
+    }
+  }
+  
+  
+  export default WizardForm;
